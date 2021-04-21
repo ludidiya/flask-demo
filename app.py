3f5fe9f805604337793066b0d63a7ef2f0c7e040
ludidiya <lingludi1995@163.com>
2021-04-22 00:35:05 +0800

    add index page and template readme file

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 # 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app
 # escape() 函数可对用户恶意输入代码进行转义
 # Flask 提供了一个 url_for 函数来生成 URL，它接受的第一个参数就是端点值，默认为视图函数的名称
+# 使用 render_template() 函数可以把模板渲染出来，必须传入的参数为模板文件名
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -25,3 +25,20 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+
+# 定义虚拟数据
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
