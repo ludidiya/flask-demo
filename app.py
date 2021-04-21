0d72ecbd41af3acaab97567e2bf98ead9aad2bbe
ludidiya <lingludi1995@163.com>
2021-04-22 00:09:37 +0800

    add a minimal home page

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, escape, url_for
+# 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app
+# escape() 函数可对用户恶意输入代码进行转义
+# Flask 提供了一个 url_for 函数来生成 URL，它接受的第一个参数就是端点值，默认为视图函数的名称
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to My Watchlist!'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
