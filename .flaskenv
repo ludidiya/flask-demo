b0d07c039d2f7ecc143a54545f1512e7c98fc8e5
ludidiya <lingludi1995@163.com>
2021-04-22 19:21:41 +0800

    Add database support with Flask-SQLAlchemy

--- .flaskenv
+++ .flaskenv
@@ -1,6 +1,6 @@
 # .flaskenv 文件：用于存储Flask命令行系统相关的公开环境变量
 
-FLASK_APP=hello.py
+FLASK_APP=app.py
 
 # FLASK_ENV 用来设置程序运行的环境，默认为 production。在开发时，我们需要开启调试模式（debug mode），及设置为development
 FLASK_ENV=development
--- app.py
+++ app.py
@@ -1,14 +1,33 @@
+import os
+import sys
 from flask import Flask, escape, url_for, render_template
 # 从 flask 包导入 Flask 类，通过实例化这个类，创建一个程序对象 app
 # escape() 函数可对用户恶意输入代码进行转义
 # Flask 提供了一个 url_for 函数来生成 URL，它接受的第一个参数就是端点值，默认为视图函数的名称
 # 使用 render_template() 函数可以把模板渲染出来，必须传入的参数为模板文件名
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+# 导入扩展类:
+# 借助 SQLAlchemy，你可以通过定义 Python 类来表示数据库里的一张表（类属性表示表中的字段 / 列）
+# 通过对这个类进行各种操作来代替写 SQL 语句。这个类我们称之为模型类，类中的属性我们将称之为字段。
 
-@app.route('/')
-def index():
-    return render_template('index.html', name=name, movies=movies)
+# 设置数据库URI
+# 为了设置 Flask、扩展或是我们程序本身的一些行为，我们需要设置和定义一些配置变量。
+# Flask 提供了一个统一的接口来写入和获取这些配置变量：Flask.config 字典。
+# 配置变量的名称必须使用大写，写入配置的语句一般会放到扩展类实例化语句之前。
+
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -17,7 +36,7 @@ def user_page(name):
 @app.route('/test')
 def test_url_for():
     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 输出：/
+    print(url_for('index'))  # 输出：/
     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
     print(url_for('user_page', name='peter'))  # 输出：/user/peter
@@ -29,16 +48,75 @@ def test_url_for():
 
 
 # 定义虚拟数据
-name = 'Diadia LING'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+# 全局变量
+# name = 'Diadia LING'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+
+# 创建数据库模型
+
+class User(db.Model):
+    # 模型类要声明继承 db.Model
+    # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+
+# 在Flask内自定义命令
+import click
+# 创建数据库表和表内虚拟数据
+@app.cli.command()  # 注册为命令
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+# 在程序里操作数据库
+# 在主页试图读取数据库记录
+@app.route('/')
+def index():
+    user = User.query.first() # 读取用户记录，返回的是数据库的对象，所以在index中还需要通过user.name来提取具体值
+    movies = Movie.query.all() # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+    #return render_template('index.html', name=name, movies=movies)
+
+# 创建一个函数来生成数据库中的虚拟数据
